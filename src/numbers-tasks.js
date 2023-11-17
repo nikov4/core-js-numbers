9c17b60750d66cdacaca2cda3b5e51dd934d95b2
Nikolai O <nik@o84.ru>
2023-11-17 11:25:19 +0300

    feat: add 54 completed tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -658,8 +658,8 @@ function getRandomInteger(min, max) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
+function getHypotenuse(a, b) {
+  return Math.hypot(a, b);
 }
 
 /**
@@ -675,8 +675,19 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  const sign = Math.sign(number);
+  let num = number;
+  if (sign !== 1) {
+    num = number * -1;
+  }
+  let count = 0;
+  for (let i = 0; i <= num; i += 1) {
+    if (i % 2 !== 0) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = {
